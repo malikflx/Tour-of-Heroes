73ce41e8f85482a8194778fda8f751f7ec271d11
Malik Felix <malikfelix50@gmail.com>
2020-03-18 14:16:11 -0400

    First commit

---
+++ src/app/toc/toc.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toc',
+  templateUrl: './toc.page.html',
+  styleUrls: ['./toc.page.scss'],
+})
+export class TocPage implements OnInit {
+  public appPages = [
+   
+    {
+      title: 'Chapter 1',
+      url: '/chapter1',
+      icon: 'book',
+      note: 'The Beginning'
+    },
+    {
+      title: 'Chapter 2',
+      url: '/chapter2',
+      icon: 'book',
+      note: 'The Second Part'
+    },
+    {
+      title: 'Chapter 3',
+      url: '/chapter3',
+      icon: 'book',
+      note: 'The Third Part'
+    },
+    {
+      title: 'Chapter 4',
+      url: '/chapter4',
+      icon: 'book',
+      note: 'Fin'
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
